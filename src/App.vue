d1669d2ca71f17eacfaca494c27db0cb60b94977
Victor-Arancibia <arancibia.vmag@hotmail.com>
2024-06-09 21:17:26 -0500

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <Contador></Contador>
+  <ListaDeTareas></ListaDeTareas>
+  <MainView></MainView>
 </template>
 
 <script setup>
+import MainView from './views/MainView.vue'
+/*
 import Contador from './components/Contador.vue';
+import ListaDeTareas from './components/ListaDeTareas.vue'
+*/
 </script>
 
 <style scoped>
